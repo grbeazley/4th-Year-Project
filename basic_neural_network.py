a84ec2905502863f2d6935ee8754a79b08495f2b
Greg Beazley <grbeazley@gmail.com>
2019-11-02 16:00:06 +0000

    Added some initial files

---
+++ basic_neural_network.py
@@ -0,0 +1,13 @@
+#
+# Implements a neural network with 12-32-1 architecture and tanh activation function
+#
+
+from utilities import sym_sigmoid, dev_sym_sigmoid
+import numpy as np
+
+# Create a dummy data vector for testing
+data = np.linspace(0, 11, 12)
+
+weights = []
+
+
---
+++ utilities.py
@@ -0,0 +1,30 @@
+import numpy as np
+import random
+
+
+def sigmoid(x):
+    """
+    Computes the sigmoid function for scalar or vector x
+    """
+    return 1 / (1 + np.exp(-x))
+
+
+def sym_sigmoid(x):
+    """
+    Computes symmetric sigmoid function based on tanh
+    Works for scalar or vector input x
+    y = a*tanh(s*x)
+    y = a - 2a / ( 1 + exp(2*s*x) )
+    """
+    a = 1.7159
+    s = 2/3
+    return a - 2*a / (1 + np.exp(2*s*x))
+
+
+def dev_sym_sigmoid(x):
+    """
+    Computes the derivative of the symmetrical sigmoid activation function
+    """
+    a = 1.7159
+    s = 2/3
+    return 4*a*s*np.exp(2*s*x) / (1 + np.exp(2*s*x)) ** 2
