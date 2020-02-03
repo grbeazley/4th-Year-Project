e957d7693600ae9f920b2bd9ca81e2d91e3c1984
grbeazley <grbeazley@gmail.com>
2020-02-03 16:08:39 +0000

    Modified particle filter to use EM in an online manner

--- particle_filter.py
+++ particle_filter.py
@@ -12,12 +12,12 @@ class ParticleFilter:
         self.num_particles = num_particles
         self.num_iterations = num_iterations
 
+        self.burn_in = 10
+
         self.true_obs = true_obs
         self.process_history = np.zeros([self.num_particles, self.num_data + 1])
-        self.params_history = np.zeros([3, self.num_iterations + 1])
+        self.params_history = np.zeros([3, self.num_data - self.burn_in])
         self.weights_history = np.zeros([self.num_particles, self.num_data + 1])
-
-        self.initial_sample = np.random.randn(self.num_particles)
 
         if 'x0' in kwargs:
             self.x_0 = kwargs['x0']
@@ -55,23 +55,26 @@ class ParticleFilter:
         log_obs = -0.5 * np.log(sigma_sqrd) - (y**2 / (2 * sigma_sqrd))
         return np.exp(log_obs)
 
-    def filter_pass(self):
-        # Run the particle filter once through the data
+    def calibrate_model(self):
+        # Calibrate the model using online EM learning
+        initial_sample = np.random.randn(self.num_particles)
 
-        initial_weights = self.observation(self.initial_sample, test_y[0])
+        initial_weights = self.observation(initial_sample, test_y[0])
         initial_weights = initial_weights / np.sum(initial_weights)
 
-        self.process_history[:, 0] = self.initial_sample
+        self.process_history[:, 0] = initial_sample
 
         weights = initial_weights
 
         self.weights_history[:, 0] = initial_weights
     
         particle_range = np.arange(self.num_particles)
+
+        self.params_history[:, 0] = [self.a, self.b, self.c]
     
         # weights_norm_constant = np.sum(initial_weights)
     
-        for i in range(self.num_data):
+        for i in tqdm(range(self.num_data)):
             # Choose which particles to continue with using their weights
             particle_indexes = np.random.choice(particle_range, size=self.num_particles, p=weights)
             Xn = self.process_history[particle_indexes, i]
@@ -97,47 +100,44 @@ class ParticleFilter:
             # Store the new normalised weights in the weights vector
             weights = self.weights_history[:, i + 1] / weights_norm_constant
 
-    def calibrate_model(self):
-        # Run the filter pass numerous times to optimise a,b,c
+    # def calibrate_model(self):
+    #     # Run the filter pass numerous times to optimise a,b,c
+    #
+    #     self.clear_history()
 
-        self.clear_history()
-        self.params_history[:, 0] = [self.a, self.b, self.c]
+            if i > self.burn_in:
+                # Allow some burn in
 
-        for i in tqdm(range(self.num_iterations)):
-            # Run filter to populate process & weights history
-            self.filter_pass()
-            final_weights = self.weights_history[:, -1]
-            final_weights_norm = final_weights / np.sum(final_weights)
+                # Calculate a' & b'
+                one_step_sum = np.sum(self.process_history[:, 1:i+1] * self.process_history[:, :i], axis=1)
+                sqrd_prdct = np.square(self.process_history)
+                sqrd_minus_one_sum = np.sum(sqrd_prdct[:, :-1], axis=1)
+                sqrd_sum = np.sum(sqrd_prdct, axis=1)
 
-            # Calculate a' & b'
-            one_step_sum = np.sum(self.process_history[:, 1:] * self.process_history[:, :-1], axis=1)
-            sqrd_prdct = np.square(self.process_history)
-            sqrd_minus_one_sum = np.sum(sqrd_prdct[:, :-1], axis=1)
-            sqrd_sum = np.sum(sqrd_prdct, axis=1)
+                # Terms as labelled in notes
+                term1 = np.dot(weights, one_step_sum)
+                term2 = np.dot(weights, sqrd_minus_one_sum)
+                term3 = np.dot(weights, sqrd_sum)
 
-            # Terms as labelled in notes
-            term1 = np.dot(final_weights_norm, one_step_sum)
-            term2 = np.dot(final_weights_norm, sqrd_minus_one_sum)
-            term3 = np.dot(final_weights_norm, sqrd_sum)
+                self.a = term1 / term2
 
-            self.a = term1 / term2
+                self.b = (term3 - (term1**2 / term2)) / (self.num_data + 1)
 
-            self.b = (term3 - (term1**2 / term2)) / (self.num_data + 1)
+                # Calculate c'
+                exp_prdct = np.square(self.true_obs) * np.exp(-self.process_history)
+                exp_sum = np.sum(exp_prdct, axis=1)
+                self.c = np.dot(weights, exp_sum) / (self.num_data + 1)
 
-            # Calculate c'
-            exp_prdct = np.square(self.true_obs) * np.exp(-self.process_history)
-            exp_sum = np.sum(exp_prdct, axis=1)
-            self.c = np.dot(final_weights_norm, exp_sum) / (self.num_data + 1)
+                # Store update to parameters
+                self.params_history[:, i - self.burn_in] = [self.a, self.b, self.c]
 
-            # Store update to parameters
-            self.params_history[:, i + 1] = [self.a, self.b, self.c]
-
-    def clear_history(self, clear_params=False):
-        # Reset history vectors to all zeros
-        if clear_params:
-            self.params_history = np.zeros([3, self.num_iterations + 1])
-        self.process_history = np.zeros([self.num_particles, self.num_data + 1])
-        self.weights_history = np.zeros([self.num_particles, self.num_data + 1])
+    # def clear_history(self, clear_params=False):
+    #     # Reset history vectors to all zeros
+    #     raise Exception("Don't want to call this atm")
+    #     if clear_params:
+    #         self.params_history = np.zeros([3, self.num_data + 1])
+    #     self.process_history = np.zeros([self.num_particles, self.num_data + 1])
+    #     self.weights_history = np.zeros([self.num_particles, self.num_data + 1])
 
     def plot_filter_pass(self):
         # Plot the result of the particle filter pass to check it worked correctly
@@ -151,18 +151,18 @@ class ParticleFilter:
         plt.legend(['a', 'b', 'c'])
 
 
-aa = 0.6
+aa = 0.8
 bb = 1
 cc = 0.5
 
 np.random.seed(0)
 
-num_data = 500
-N = 200
+num_data = 1000
+N = 500
 
 test_x, test_y = gen_univ_sto_vol(num_data, a=aa, b=bb, c=cc, return_hidden=True)
 
-particle_filter = ParticleFilter(test_y, num_particles=N, a=0.95, b=0.5, c=0.1, num_iterations=10)
+particle_filter = ParticleFilter(test_y, num_particles=N, a=0.5, b=0.5, c=1, num_iterations=10)
 # particle_filter.filter_pass()
 # particle_filter.plot_filter_pass()
 particle_filter.calibrate_model()
